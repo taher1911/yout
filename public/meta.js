42236935cc81d71284691b7c3a42fa10cfcbbfb8
taher1911 <72612843+taher1911@users.noreply.github.com>
2022-04-22 11:16:54 +0200

    Add files via upload

---
+++ public/meta.js
@@ -0,0 +1,120 @@
+const meta = (
+  <>
+    <meta name="keywords" content="video ,mp3,  download videos" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="subject" content="youtube shorts downloader" />
+    <meta name="copyright" content="youtube shorts downloader" />
+    <meta name="robots" content="index,follow" />
+    <meta name="revised" content="Friday, July 18th, 2022, 5:15 pm" />
+    <meta name="abstract" content="youtube shorts downloader" />
+    <meta name="topic" content="youtube shorts downloader" />
+    <meta name="summary" content="youtube shorts downloader" />
+    <meta name="Classification" content="Business" />
+    <meta name="author" content="name, email@hotmail.com" />
+    <meta name="designer" content="youtube shorts downloader" />
+    <meta name="reply-to" content="email@hotmail.com" />
+    <meta name="owner" content="youtube shorts downloader" />
+    <meta name="url" content="https://ytshorts.savetube.me/" />
+    <meta name="identifier-URL" content="https://ytshorts.savetube.me/" />
+    <meta name="directory" content="submission" />
+    <meta name="category" content="youtube shorts downloader" />
+    <meta name="coverage" content="Worldwide" />
+    <meta name="distribution" content="Global" />
+    <meta name="rating" content="General" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="subtitle" content="youtube shorts downloader" />
+    <meta name="target" content="all" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="MobileOptimized" content="320" />
+    <meta name="date" content="May. 22, 2022" />
+    <meta name="search_date" content="2022-04-22" />
+    <meta name="DC.title" content="youtube shorts downloader" />
+    <meta
+      name="ResourceLoaderDynamicStyles"
+      content="youtube shorts downloader"
+    />
+    <meta name="medium" content="blog" />
+    <meta name="syndication-source" content="https://ytshorts.savetube.me/" />
+    <meta name="original-source" content="https://ytshorts.savetube.me/" />
+    <meta
+      name="verify-v1"
+      content="dV1r/ZJJdDEI++fKJ6iDEl6o+TMNtSu0kv18ONeqM0I="
+    />
+    <meta name="y_key" content="1e39c508e0d87750" />
+    <meta name="pageKey" content="guest-home" />
+    <meta itemProp="name" content="jQTouch" />
+    <meta httpEquiv="Expires" content="0" />
+    <meta httpEquiv="Pragma" content="no-cache" />
+    <meta httpEquiv="Cache-Control" content="no-cache" />
+    <meta httpEquiv="imagetoolbar" content="no" />
+    <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+    <meta httpEquiv="x-dns-prefetch-control" content="off" />
+
+    <meta name="og:url" content="https://ytshorts.savetube.me/" />
+    <meta
+      name="og:image"
+      content="https://ytshorts.savetube.me/static/staticImage/og.jpg"
+    />
+
+    <meta name="fb:page_id" content="43929265776" />
+    <meta name="application-name" content="youtube shorts downloader" />
+    <meta name="og:email" content="me@example.com" />
+    <meta name="og:phone_number" content="650-123-4567" />
+    <meta name="og:fax_number" content="+1-415-123-4567" />
+    <meta name="og:latitude" content="37.416343" />
+    <meta name="og:longitude" content="-122.153013" />
+    <meta name="og:street-address" content="1601 S California Ave" />
+    <meta name="og:locality" content="Palo Alto" />
+    <meta name="og:region" content="CA" />
+    <meta name="og:postal-code" content="94304" />
+    <meta name="og:country-name" content="USA" />
+    <meta property="fb:admins" content="987654321" />
+    <meta property="og:type" content="youtube shorts downloader" />
+    <meta property="og:points" content="POINTS_FOR_ACHIEVEMENT" />
+    <meta property="og:video" content="https://ytshorts.savetube.me/" />
+    <meta property="og:video:height" content="640" />
+    <meta property="og:video:width" content="385" />
+    <meta property="og:video:type" content="application/x-shockwave-flash" />
+    <meta property="og:video" content="https://ytshorts.savetube.me/" />
+    <meta property="og:video:type" content="video/mp4" />
+    <meta property="og:video" content="https://ytshorts.savetube.me/" />
+    <meta property="og:video:type" content="text/html" />
+    <meta property="og:audio" content="https://ytshorts.savetube.me/" />
+    <meta property="og:audio:title" content="Amazing Song" />
+    <meta property="og:audio:artist" content="Amazing Band" />
+    <meta property="og:audio:album" content="Amazing Album" />
+    <meta property="og:audio:type" content="application/mp3" />
+    <meta
+      name="microid"
+      content="mailto+http:sha1:e6058ed7fca4a1921cq91d7f1f3b8736cd3cc1g7"
+    />
+    <meta
+      name="readability-verification"
+      content="E7aEHvVQpWc8VHDqKvaB2Z58hek2EAv2HuLuegv7"
+    />
+    <meta
+      name="google-site-verification"
+      content="4SMIedO1X4IkYrYuhEC2VuovdQM36Xxb0btUjElqQyg"
+    />
+    <meta name="ICBM" content="40.746990, -73.980537" />
+    <meta name="generator" content="Nextjs ,Tailwindcss" />
+    <meta
+      name="norton-safeweb-site-verification"
+      content="tz8iotmk-pkhui406y41y5bfmfxdwmaa4a-yc0hm6r0fga7s6j0j27qmgqkmc7oovihzghbzhbdjk-uiyrz438nxsjdbj3fggwgl8oq2nf4ko8gi7j4z7t78kegbidl4"
+    />
+    <meta
+      name="apple-mobile-web-app-title"
+      content="youtube shorts downloader"
+    />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-touch-fullscreen" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="format-detection" content="telephone=no" />
+    <link
+      rel="icon"
+      href="https://ytshorts.savetube.me/static/staticImage/og.jpg"
+    />
+  </>
+);
+
+export default meta;
